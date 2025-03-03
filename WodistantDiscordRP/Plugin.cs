ba3f7d1fc44054f14382bfafb1227e9b031e35f6
yumu <93029942+yumunet@users.noreply.github.com>
2025-03-03 19:12:01 +0900

    init

---
+++ WodistantDiscordRP/Plugin.cs
@@ -0,0 +1,28 @@
+﻿using Wodistant.PluginLibrary.Hook.Keyboard;
+using Wodistant.PluginLibrary.MenuBar;
+
+namespace WodistantDiscordRP
+{
+    public class Plugin : Wodistant.PluginLibrary.PluginBase
+    {
+        public override string Name => "Discord Rich Presence";
+
+        public override string Author => "yumu";
+
+        public override string Version => "1.0.0";
+
+        public override string Description => "Discordクライアントと連携して、アクティビティステータス（Rich Presence）を表示させます。";
+
+        public override MenuBarInfo ManuBarInfomation
+        {
+            get
+            {
+                return null;
+            }
+        }
+
+        public override IKeyboardAction[] KeyboardActions => new IKeyboardAction[]
+        {
+        };
+    }
+}
---
+++ WodistantDiscordRP/Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("WodistantDiscordRP")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WodistantDiscordRP")]
+[assembly: AssemblyCopyright("Copyright © 2025 yumu")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("0bfd6517-e09a-40d4-b6db-2ce518bac95c")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
